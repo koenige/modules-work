d26bba0b64316f60998e95d76c4c1311fb32c49e
Gustaf Mossakowski <gustaf@koenige.org>
2022-03-18 12:56:35 +0100

    tasks: adjust table to me.local configuration; add field todos.created; add tag filter; add period filter

--- zzbrick_tables/tasks.php
+++ zzbrick_tables/tasks.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/work
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2021-2022 Gustaf Mossakowski
+ * @copyright Copyright © 2009-2013, 2017, 2019-2022 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
  
@@ -38,7 +38,7 @@ $zz['fields'][3]['list_suffix'] = '</div>';
 $zz['fields'][22] = zzform_include_table('tasks-categories');
 $zz['fields'][22]['title'] = 'Tags';
 $zz['fields'][22]['type'] = 'subtable';
-$zz['fields'][22]['min_records'] = 0;
+$zz['fields'][22]['min_records'] = 1;
 $zz['fields'][22]['max_records'] = 10;
 $zz['fields'][22]['form_display'] = 'lines';
 $zz['fields'][22]['fields'][2]['type'] = 'foreign_key';
@@ -50,12 +50,14 @@ $zz['fields'][22]['subselect']['sql'] = sprintf('SELECT todo_id
 	LEFT JOIN categories main_categories
 		ON categories.main_category_id = main_categories.category_id
 		AND main_categories.category_id != %d
-', wrap_category_id('todos'));
+', wrap_category_id('tasks'));
 $zz['fields'][22]['subselect']['concat_fields'] = ', ';
+$zz['fields'][22]['hide_in_list_if_empty'] = true;
 
 $zz['fields'][23] = zzform_include_table('tasks-contacts');
 $zz['fields'][23]['title'] = 'Task for';
 $zz['fields'][23]['type'] = 'subtable';
+$zz['fields'][23]['hide_in_list_if_empty'] = true;
 $zz['fields'][23]['min_records'] = 0;
 $zz['fields'][23]['max_records'] = 10;
 $zz['fields'][23]['form_display'] = 'lines';
@@ -65,6 +67,8 @@ $zz['fields'][23]['subselect']['sql'] = 'SELECT todo_id, contact
 	LEFT JOIN contacts USING (contact_id)
 ';
 $zz['fields'][23]['subselect']['concat_fields'] = ', ';
+
+$zz['fields'][4] = []; // link to projects
 
 $zz['fields'][5]['title_tab'] = '<abbr title="Priority">!</abbr>';
 $zz['fields'][5]['field_name'] = 'priority';
@@ -83,25 +87,25 @@ if (!empty($_GET['filter']['type']) AND $_GET['filter']['type'] === 'none') {
 $zz['fields'][10]['title_tab'] = 'Seq.';
 $zz['fields'][10]['field_name'] = 'sequence';
 $zz['fields'][10]['type'] = 'number';
+$zz['fields'][10]['hide_in_list_if_empty'] = true;
 
 $zz['fields'][6]['field_name'] = 'deadline';
 $zz['fields'][6]['type'] = 'date';
-$zz['fields'][6]['default'] = date('d.m.Y');
+$zz['fields'][6]['hide_in_list_if_empty'] = true;
 if (!empty($_GET['filter']['type']) AND $_GET['filter']['type'] === 'none') {
 	$zz['fields'][6]['hide_in_list'] = true;
 }
 $zz['fields'][6]['group_in_list'] = true;
 
-$zz['fields'][65]['field_name'] = 'sort_order';
-$zz['fields'][65]['type'] = 'display';
-$zz['fields'][65]['hide_in_form'] = true;
-$zz['fields'][65]['hide_in_list'] = true;
-$zz['fields'][65]['group_in_list'] = true;
-$zz['fields'][65]['exclude_from_search'] = true;
-
 $zz['fields'][8]['field_name'] = 'time';
 $zz['fields'][8]['type'] = 'time';
 $zz['fields'][8]['hide_in_list'] = true;
+
+$zz['fields'][12]['field_name'] = 'created';
+$zz['fields'][12]['type'] = 'hidden';
+$zz['fields'][12]['type_detail'] = 'date';
+$zz['fields'][12]['default'] = date('Y-m-d');
+$zz['fields'][12]['hide_in_list'] = true;
 
 $zz['fields'][9]['title'] = 'Done on';
 $zz['fields'][9]['field_name'] = 'done';
@@ -112,14 +116,15 @@ $zz['fields'][11]['title'] = 'Author';
 $zz['fields'][11]['field_name'] = 'author_contact_id';
 $zz['fields'][11]['type'] = 'write_once';
 $zz['fields'][11]['type_detail'] = 'select';
-$zz['fields'][11]['sql'] = 'SELECT contact_id
+$zz['fields'][11]['sql'] = sprintf('SELECT contact_id
 		, contact, identifier
 	FROM contacts
-	ORDER BY contact';
+	WHERE contact_category_id = %d
+	ORDER BY contact', wrap_category_id('contacts/person'));
 $zz['fields'][11]['key_field_name'] = 'contact_id';
 $zz['fields'][11]['exclude_from_search'] = true;
 $zz['fields'][11]['hide_in_list'] = true;
-$zz['fields'][11]['default'] = $_SESSION['contact_id'];
+$zz['fields'][11]['default'] = $_SESSION['user_id'];
 
 /*
 $zz['fields'][12]['field_name'] = 'reminder';
@@ -131,6 +136,13 @@ $zz['fields'][12]['default'] = 'nein';
 $zz['fields'][12]['if']['add']['default'] = 'yes';
 $zz['fields'][12]['explanation'] = 'If “yes”, the task is sent as an email to the people involved.';
 */
+
+$zz['fields'][65]['field_name'] = 'sort_order';
+$zz['fields'][65]['type'] = 'display';
+$zz['fields'][65]['hide_in_form'] = true;
+$zz['fields'][65]['hide_in_list'] = true;
+$zz['fields'][65]['group_in_list'] = true;
+$zz['fields'][65]['exclude_from_search'] = true;
 
 $zz['sql'] = 'SELECT todos.*
 	, IF(deadline, "A with deadline", "B without deadline") AS sort_order
@@ -152,19 +164,57 @@ if (empty($_GET['where']['project_id'])) {
 	$zz['filter'][1]['selection']['!none'] = wrap_text('Task');
 	$zz['filter'][1]['default_selection'] = '!none';
 
-	$zz['filter'][2]['title'] = wrap_text('Status');
-	$zz['filter'][2]['identifier'] = 'status';
-	$zz['filter'][2]['type'] = 'list';
-	$zz['filter'][2]['where'] = 'done';
-	$zz['filter'][2]['selection']['NULL'] = wrap_text('pending');
-	$zz['filter'][2]['selection']['!NULL'] = wrap_text('done');
-	$zz['filter'][2]['default_selection'] = 'NULL';
+	$zz['filter'][5]['title'] = wrap_text('Status');
+	$zz['filter'][5]['identifier'] = 'status';
+	$zz['filter'][5]['type'] = 'list';
+	$zz['filter'][5]['where'] = 'done';
+	$zz['filter'][5]['selection']['NULL'] = wrap_text('pending');
+	$zz['filter'][5]['selection']['!NULL'] = wrap_text('done');
+	$zz['filter'][5]['default_selection'] = 'NULL';
 
 /*
-	$zz['filter'][3]['title'] = 'Persons';
-	$zz['filter'][3]['identifier'] = 'person';
-	$zz['filter'][3]['type'] = 'list';
-	$zz['filter'][3]['where'] = 'contact_id';
-	$zz['filter'][3]['selection'][$_SESSION['contact_id']] = 'Own';
+	$zz['filter'][6]['title'] = 'Persons';
+	$zz['filter'][6]['identifier'] = 'person';
+	$zz['filter'][6]['type'] = 'list';
+	$zz['filter'][6]['where'] = 'contact_id';
+	$zz['filter'][6]['selection'][$_SESSION['contact_id']] = 'Own';
 */
 }
+
+$zz['filter'][3]['title'] = wrap_text('Tag');
+$zz['filter'][3]['sql'] = 'SELECT DISTINCT category_id, SUBSTRING(path, 6)
+	FROM todos_categories
+	LEFT JOIN categories USING (category_id)
+	LEFT JOIN todos USING (todo_id)
+	%s
+	ORDER BY SUBSTRING(path, 6)
+';
+$zz['filter'][3]['identifier'] = 'tag';
+$zz['filter'][3]['type'] = 'list';
+$zz['filter'][3]['sql_join'] = 'LEFT JOIN todos_categories USING (todo_id)';
+$zz['filter'][3]['where'] = 'todos_categories.category_id';
+
+$zz['filter'][4]['title'] = wrap_text('Period');
+$zz['filter'][4]['identifier'] = 'period';
+$zz['filter'][4]['type'] = 'list';
+$zz['filter'][4]['where_if'][0] = '';
+$zz['filter'][4]['where_if'][1] = 'YEARWEEK(deadline, 1) < YEARWEEK(DATE_SUB(CURDATE(), INTERVAL 28 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][2] = 'YEARWEEK(deadline, 1) < YEARWEEK(DATE_SUB(CURDATE(), INTERVAL 7 DAY), 1) AND YEARWEEK(deadline, 1) >= YEARWEEK(DATE_SUB(CURDATE(), INTERVAL 28 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][3] = 'YEARWEEK(deadline, 1) = YEARWEEK(DATE_SUB(CURDATE(), INTERVAL 7 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][4] = 'YEARWEEK(deadline, 1) = YEARWEEK(CURDATE(), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][5] = 'YEARWEEK(deadline, 1) = YEARWEEK(DATE_ADD(CURDATE(), INTERVAL 7 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][6] = 'YEARWEEK(deadline, 1) > YEARWEEK(DATE_ADD(CURDATE(), INTERVAL 7 DAY), 1) AND YEARWEEK(deadline, 1) <= YEARWEEK(DATE_ADD(CURDATE(), INTERVAL 28 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][7] = 'YEARWEEK(deadline, 1) > YEARWEEK(DATE_ADD(CURDATE(), INTERVAL 28 DAY), 1) AND ISNULL(done)';
+$zz['filter'][4]['where_if'][8] = 'ISNULL(deadline) AND ISNULL(done)';
+$zz['filter'][4]['selection'][1] = wrap_text('long ago');
+$zz['filter'][4]['selection'][2] = wrap_text('minus four weeks');
+$zz['filter'][4]['selection'][3] = wrap_text('last week');
+$zz['filter'][4]['selection'][4] = wrap_text('this week');
+$zz['filter'][4]['selection'][5] = wrap_text('next week');
+$zz['filter'][4]['selection'][6] = wrap_text('plus four weeks');
+$zz['filter'][4]['selection'][7] = wrap_text('far away');
+$zz['filter'][4]['selection'][8] = wrap_text('no deadline');
+$zz['filter'][4]['default_selection'] = 4;
+
+$zz_conf['limit'] = 40;
+$zz_conf['search_form_always'] = true;
