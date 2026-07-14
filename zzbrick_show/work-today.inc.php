1b5792f1ccf4a969e8298843ab867a4dcd95112d
Gustaf Mossakowski <gustaf@koenige.org>
2026-07-14 19:49:18 +0200

    work-today: add show work-today block, route for worklogs table; update translations

---
+++ zzbrick_show/work-today.inc.php
@@ -0,0 +1,44 @@
+<?php 
+
+/**
+ * work module
+ * show work from today
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/work
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2006-2012, 2017, 2020, 2025-2026 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+ 
+
+function mod_work_show_work_today() {
+	$sql = 'SELECT worklog_id, event_id, event, identifier, work
+			, DATE_FORMAT(work_begin, "%%H:%%i") AS begin
+			, DATE_FORMAT(work_end, "%%H:%%i") AS end
+			, TIMESTAMPDIFF(MINUTE, work_begin, work_end) AS duration
+			, CONCAT(TIMESTAMPDIFF(HOUR, work_begin, work_end), ":", LPAD(MOD(TIMESTAMPDIFF(MINUTE, work_begin, work_end), 60), 2, 0)) AS duration_time
+		FROM worklogs
+		LEFT JOIN events USING (event_id)
+		WHERE work_begin BETWEEN CONCAT(CURDATE(), " 00:00:00") AND  CONCAT(CURDATE(), " 23:59:59")
+		OR work_end BETWEEN CONCAT(CURDATE(), " 00:00:00") AND  CONCAT(CURDATE(), " 23:59:59")
+		AND contact_id = %d
+		ORDER BY work_begin
+	';
+	$sql = sprintf($sql, $_SESSION['user_id']);
+	$work = wrap_db_fetch($sql, 'worklog_id');
+	if (empty($work)) $work['no_work'] = true;
+	$work['sum_duration'] = 0;
+	$work['sum_projects'] = [];
+	foreach ($work as $id => $line) {
+		if (!is_numeric($id)) continue;
+		$work['sum_duration'] += $line['duration'];
+		$work['sum_projects'][$line['event_id']] = $line['event_id'];
+	}
+	$work['sum_duration'] = sprintf('%d:%02d', floor($work['sum_duration'] / 60), $work['sum_duration'] % 60);
+	$work['sum_projects'] = count($work['sum_projects']);
+	$page['text'] = wrap_template('work-today', $work);
+	$page['text'] = str_replace('%%% ', '%%% explain ', $page['text']);
+	return $page;
+}
